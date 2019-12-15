7765bf421c914399b7a6865626f32ecfffbe06a9
Giona Casiraghi <giona@ethz.ch>
2019-12-15 23:11:15 +0100

    added phi_matrix functions and methods

---
+++ R/phi_matrix.R
@@ -0,0 +1,95 @@
+#' Compute Phi Matrix
+#'
+#' @param graph n x n adjacency matrix of the graph of which computing the phi matrix
+#' @param model ghype model against which computing the phi matrix
+#' @param lightMemory boolean, store model and graph? default FALSE
+#'
+#' @return n x n phi matrix
+#' @export
+#'
+#' @examples
+#' 
+#' #' data("adj_karate")
+#' model <- scm(graph = adj_karate, directed = FALSE, selfloops = FALSE)
+#' (phi <- phi_matrix(graph = adj_karate, model = model))
+#' plot(phi)
+#' 
+phi_matrix <- function(graph, model, lightMemory=FALSE){
+  # compute under and over represented dyads
+  over <- linkSignificance(graph = graph, model=model, under=FALSE)
+  under <- linkSignificance(graph = graph, model=model, under=TRUE)
+  
+  #compute phi
+  phi_mat <- over - under
+  
+  if(lightMemory){
+    phi <- list(matrix=phi_mat, graph=NULL, model=NULL)
+  } else{
+    phi <- list(matrix=phi_mat, graph=graph, model=model)
+  }
+  class(phi) <- append('phi_matrix',class(phi))
+  
+  return(phi)
+}
+
+
+#' Print method for objects of class phi_matrix
+#'
+#' @param x an object of class phi_matrix
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @export
+#'
+#' @examples
+#' data("adj_karate")
+#' model <- scm(graph = adj_karate, directed = FALSE, selfloops = FALSE)
+#' phi <- phi_matrix(graph = adj_karate, model = model)
+#' print(phi)
+#' 
+print.phi_matrix <- function(x, ...){
+  rownames(phi$matrix) <- colnames(phi$matrix) <- rownames(phi$graph)
+  print(phi$matrix)
+}
+
+#' Plot method for objects of class phi_matrix
+#'
+#' @param x an object of class phi_matrix
+#' @param ... arguments to be passed to methods, such as graphical parameters
+#'
+#' @export
+#' @example 
+#' data("adj_karate")
+#' model <- scm(graph = adj_karate, directed = FALSE, selfloops = FALSE)
+#' phi <- phi_matrix(graph = adj_karate, model = model)
+#' plot(phi)
+#' 
+plot.phi_matrix <- function(x, ...){
+  rownames(phi$matrix) <- colnames(phi$matrix) <- rownames(phi$graph)
+  hmcol<-RColorBrewer::brewer.pal(11,"RdBu")
+  heatmap(phi$matrix, symm = TRUE, col=hmcol, ...)
+}
+
+
+# rownames(adj_karate)
+# labels <- c(1,1,1,1,2,2,2,1,3,3,2,1,1,1,3,3,2,1,3,1,3,1,3,3,3,3,3,3,3,3,3,3,3,3)
+# AIC(bccm(adj_karate,labels,F,F))
+# data("vertexlabels")
+# AIC(bccm(adj_karate,vertexlabels,F,F))
+# newmod <- bccm(adj_karate,labels,F,F)
+# 
+# library(ggraph)
+# 
+# ggraph(igraph::graph_from_adjacency_matrix(phi$matrix, mode = 'undir', weighted = T), 
+#        layout=create_layout(adj_karate, layout = 'auto')[,c('x','y')]) +
+#   geom_edge_link(aes(alpha=abs(weight), colour=weight)) +
+#   scale_edge_colour_distiller(palette = 'RdBu') +
+#   geom_node_point(aes(colour=as.factor(labels), size=apply(adj_karate,2,sum)))
+# 
+# phi <- phi_matrix(graph = adj_karate, model = newmod)
+# plot(phi)
+# 
+# ggraph(igraph::graph_from_adjacency_matrix(phi$matrix, mode = 'undir', weighted = T), 
+#        layout=create_layout(adj_karate, layout = 'auto')[,c('x','y')]) +
+#   geom_edge_link(aes(alpha=abs(weight), colour=weight)) +
+#   scale_edge_colour_distiller(palette = 'RdBu') +
+#   geom_node_point(aes(colour=as.factor(labels), size=apply(adj_karate,2,sum)))
